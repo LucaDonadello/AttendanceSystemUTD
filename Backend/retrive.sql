933e1265c46a7f565584b65253ed2ddb6f295220
Luca Donadello <luca.donadello99@gmail.com>
2024-03-27 18:19:45 -0500

    update create.sql
    add insert.sql for testing purposes

---
+++ Backend/retrive.sql
@@ -0,0 +1,11 @@
+use AttendanceApp;
+
+select * from Student;
+select * from Professor;
+select * from QuizQuestion;
+select * from StudentAnswer;
+select * from QuizBank; 
+select * from Quiz; 
+select * from Course;
+select * from Attendance;
+select * from Password;
