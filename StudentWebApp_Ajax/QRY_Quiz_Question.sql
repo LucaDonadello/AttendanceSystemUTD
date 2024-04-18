36a8b25de1d9ad60e9b794f33f5eb55a398c8325
Samuel Benicewicz <64711289+SamB133@users.noreply.github.com>
2024-04-17 22:12:39 -0500

    add comments

--- StudentWebApp_Ajax/QRY_Quiz_Question.sql
+++ StudentWebApp_Ajax/QRY_Quiz_Question.sql
@@ -1,3 +1,4 @@
+/* Samuel Benicewicz // Query for getting the quiz questions once a person logs in successfully */
 SELECT QuizID, QuizBankID, Question, AnswerSet, CorrectAnswer FROM Quiz
 INNER JOIN QuizBank ON Quiz.QuizBankID = QuizBank.QuestionBankID
 INNER JOIN QuizQuestion ON QuizBank.QuizQuestionID = QuizQuestion.QuestionID
--- StudentWebApp_Ajax/QRY_Student_Login.sql
+++ StudentWebApp_Ajax/QRY_Student_Login.sql
@@ -1,3 +1,4 @@
+/* Samuel Benicewicz // Query for confirming a student is in the course and logging them in if so */
 SELECT FirstName, LastName, QuizID FROM Student
 INNER JOIN Course on Student.StudentUTDID = Course.StudentUTDID
 INNER JOIN Password on Course.CourseID = Password.ClassID
