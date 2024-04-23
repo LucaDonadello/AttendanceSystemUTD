f20d85f3f4e8a46223632d059782e3228d74161b
Samuel Benicewicz <64711289+SamB133@users.noreply.github.com>
2024-04-23 05:43:21 -0500

    Delete StudentWebApp_Ajax directory

--- StudentWebApp_Ajax/QRY_Student_Login.sql
+++
@@ -1,5 +0,0 @@
-/* Samuel Benicewicz // Query for confirming a student is in the course and logging them in if so */
-SELECT FirstName, LastName, QuizID FROM Student
-INNER JOIN Course on Student.StudentUTDID = Course.StudentUTDID
-INNER JOIN Password on Course.CourseID = Password.ClassID
-WHERE Student.StudentUTDID = 123456789 AND Password.Password_ = 'password123'
