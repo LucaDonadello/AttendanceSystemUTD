15d006d3a419e8ab8659a5ceae7e9f19fe87f0a6
Samuel Benicewicz <64711289+SamB133@users.noreply.github.com>
2024-04-26 23:15:59 -0500

    remove old files

--- StudentWebApp/Frontend/login.php
+++
@@ -1,7 +0,0 @@
-<?php // Samuel Benicewicz // PHP needed for login page until app is deployed
-if ($_POST['utd-id'] == 'admin' && $_POST['pwd'] == 'admin') {
-    echo "true";
-} else {
-    echo "false";
-}
-?>
